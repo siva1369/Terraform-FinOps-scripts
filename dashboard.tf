634661acdb89273b36556aa845aa640c00e58d7d
Sivaiah Addanki <100773996+siva1369@users.noreply.github.com>
2026-02-23 11:27:53 +0530

    Update dashboard.tf

--- dashboard.tf
+++ dashboard.tf
@@ -1,58 +1,29 @@
-# resource "aws_cloudformation_stack" "dashboard" {
-
-#   name = var.dashboard_stack
-
-#   template_url = "https://raw.githubusercontent.com/aws-samples/aws-cloud-intelligence-dashboards/main/cfn/cloud-intelligence-dashboard.json"
-
-#   capabilities = [
-
-#     "CAPABILITY_IAM",
-#     "CAPABILITY_NAMED_IAM",
-#     "CAPABILITY_AUTO_EXPAND"
-#   ]
-
-#   parameters = {
-
-#     CURDatabaseName = aws_glue_catalog_database.database.name
-
-#     CURTableName = "cost_and_usage_report"
-
-#     AthenaWorkgroup = aws_athena_workgroup.athena.name
-#   }
-
-#   depends_on = [
-#     aws_glue_crawler.crawler
-#   ]
-# }
-
-
 resource "aws_cloudformation_stack" "dashboard" {
 
   name = var.dashboard_stack
 
-  template_url = "https://${aws_s3_bucket.cur_bucket.bucket}.s3.${var.region}.amazonaws.com/templates/cloud-intelligence-dashboard.json"
+  template_url = "https://aws-cloud-intelligence-dashboards.s3.amazonaws.com/cfn/cudos/cudos.yaml"
 
   capabilities = [
-
-    "CAPABILITY_IAM",
-    "CAPABILITY_NAMED_IAM",
-    "CAPABILITY_AUTO_EXPAND"
+    "CAPABILITY_NAMED_IAM"
   ]
 
   parameters = {
 
-    CURDatabaseName = aws_glue_catalog_database.database.name
-
-    CURTableName = "cost_and_usage_report"
-
+    # REQUIRED
     AthenaWorkgroup = aws_athena_workgroup.athena.name
+    CURDatabaseName = aws_glue_catalog_database.database.name
+    CURTableName    = "cost_and_usage_report"
+
+    # REQUIRED for QuickSight
+    QuickSightUser = "default/sivaiah123@gmail.com"
+
+    # REQUIRED region
+    QuickSightRegion = var.region
+
   }
 
   depends_on = [
-
-    aws_s3_object.dashboard_template,
     aws_glue_crawler.crawler
   ]
-
-  
 }
